21d5f450bca6a2348f524b88aad1c0fc4886310d
Carl <me@songlin.me>
2024-06-08 19:43:53 +0800

    Update dominos.js

--- rewrite/dominos.js
+++ rewrite/dominos.js
@@ -1,4 +1,3 @@
-;^http://example.com/resource1/4/ url reject-dict
 http-request ^https:\/\/game\.dominos\.com\.cn\/.+\/game\/gameDone script-path=https://gist.githubusercontent.com/Sliverkiss/6b4da0d367d13790a9fd1d928c82bdf8/raw/dlm.js,requires-body=true, timeout=10, tag=达美乐披萨获取token
 
 [MITM]
