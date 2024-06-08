c81a704103505b0e24c1c33c57fe119e534ec8e1
Carl <me@songlin.me>
2024-06-08 19:43:33 +0800

    Update 95598.js

--- rewrite/95598.js
+++ rewrite/95598.js
@@ -1,9 +1,5 @@
-
- 
-;^http://example.com/resource1/4/ url reject-dict
-
-
 [mimt]
 hostname = osg-open.sgcc.com.cn
+ 
 [rewrite_local]
 ^https?:\/\/osg-open\.sgcc\.com\.cn\/osg-open-p0001\/member\/c5\/f05 url script-request-header https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/95598/95598.weapp.js
