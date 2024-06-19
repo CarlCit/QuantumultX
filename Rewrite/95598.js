89e2cd9c7c6cf4f3eb0f25abbff2c3a64e09a4fe
Carl <me@songlin.me>
2024-06-19 13:25:45 +0800

    Update 95598.js

--- Rewrite/95598.js
+++ Rewrite/95598.js
@@ -2,4 +2,4 @@
 hostname = osg-open.sgcc.com.cn
  
 [rewrite_local]
-^https?:\/\/osg-open\.sgcc\.com\.cn\/osg-open-p0001\/member\/c5\/f05 url script-request-header https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/95598/95598.weapp.js
+^https?:\/\/osg-open\.sgcc\.com\.cn\/osg-open-p0001\/member\/c5\/f05 url script-request-header https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/95598/95598.js
