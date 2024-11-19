15ed42e91e6ee03aaf9bd8aa4254d6f866d2f992
Carl <me@songlin.me>
2024-11-19 16:07:19 +0800

    Create 10086_2.js

---
+++ Rewrite/10086_2.js
@@ -0,0 +1,3 @@
+^https?:\/\/api\.example\.com\/10086\/query url script-analyze-echo-response https://raw.githubusercontent.com/CarlCit/QuantumultX/refs/heads/main/Rewrite/10086.js
+
+hostname = api.example.com
