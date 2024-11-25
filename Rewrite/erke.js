3202cf96489d329bcbb93c5de69c089afa47b7a5
Carl <me@songlin.me>
2024-11-25 22:05:39 +0800

    Update erke.js

--- Rewrite/erke.js
+++ Rewrite/erke.js
@@ -2,4 +2,4 @@
 hostname = hope.demogic.com
 
 [rewrite_local]
-^https:\/\/hope\.demogic\.com\/gic-wx-app\/get-member-asset\.json url https://raw.githubusercontent.com/Former-Years/Surge/refs/heads/main/Script/erke.js
+https:\/\/hope\.demogic\.com\/gic-wx-app\/get-member-asset\.json url https://raw.githubusercontent.com/Former-Years/Surge/refs/heads/main/Script/erke.js
