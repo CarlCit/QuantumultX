62c355627f47489b95ecc6a1d1d160439d260759
Carl <me@songlin.me>
2024-11-25 22:04:37 +0800

    Create erke.js

---
+++ Rewrite/erke.js
@@ -0,0 +1,5 @@
+[MITM]
+hostname = hope.demogic.com
+
+[rewrite_local]
+^https:\/\/hope\.demogic\.com\/gic-wx-app\/get-member-asset\.json url https://raw.githubusercontent.com/Former-Years/Surge/refs/heads/main/Script/erke.js
