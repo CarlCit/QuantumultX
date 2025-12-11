97936e1313911e3eeeb1c194993987908bb804f2
Carl <me@songlin.me>
2025-12-11 10:53:46 +0800

    Update iTunes.js script URL for response body

--- Rewrite/iTunes.js
+++ Rewrite/iTunes.js
@@ -12,7 +12,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/CarlCit/QuantumultX/refs/heads/main/Rewrite/iTunes.js
 
 [mitm]
 hostname = buy.itunes.apple.com
