c8f13da4a8ecae3bc76b3875a6ecc75073a1c47e
Carl <me@songlin.me>
2025-12-11 11:09:39 +0800

    Update Bear.js

--- Rewrite/Bear.js
+++ Rewrite/Bear.js
@@ -2,21 +2,13 @@
 
 /******************************
 
-脚本功能：熊掌记- Markdown+解锁订阅
-下载地址：http://mtw.so/6bTERH
-软件版本：1.9.7
-脚本作者：彭于晏
-更新时间：2022-9-14
-问题反馈：QQ+89996462
-QQ会员群：779392027
-TG反馈群：https://t.me/plus8889
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/xzj.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/CarlCit/QuantumultX/main/Rewrite/Bear.js
 
 [mitm] 
 
